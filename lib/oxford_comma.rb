8d314c4bf2847a475165b4f96d15ff0c89a85f78
Mike McNeeley <mmcneeley@gmail.com>
2018-03-01 02:42:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+require 'pry'
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 
+def oxford_comma(array)
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    array.insert(-2, "and")
+    array.join(" ")
+  else
+    array2 = array.pop()
+    array2.unshift(" and")
+    puts array.join(" ")
+  end
 end
+
+oxford_comma(array)
